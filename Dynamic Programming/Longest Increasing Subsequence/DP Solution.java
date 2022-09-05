0c4a1db067545bcb17f2913ee13c438d8c8da248
Muhammed Sabbah <30741844+muhammedSabbah@users.noreply.github.com>
2022-09-05 22:51:03 +0200

    Create DP Solution.java

---
+++ Dynamic Programming/Longest Increasing Subsequence/DP Solution.java
@@ -0,0 +1,34 @@
+package com.google;
+
+public class LongestIncreasingSubSeq {
+
+	// 5, 2, 7, 3, 4, 6
+	static int[][] memo;
+
+	public static int LISMemo(int[] elements, int current, int n, int prev) {
+		if (current == n) {
+			return 0;
+		}
+
+		if (memo[current][prev + 1] != 0) {
+			return memo[current][prev + 1];
+		}
+
+		int choice1 = 0;
+		if (prev == -1 || elements[current] > elements[prev]) {
+			choice1 = 1 + LISMemo(elements, current + 1, n, current);
+		}
+
+		int choice2 = LISMemo(elements, current + 1, n, prev);
+
+		return memo[current][prev + 1] = Math.max(choice1, choice2);
+	}
+
+	public static void main(String[] args) {
+		int[] numbers = { 1, 3, 5, 4, 7 };
+		int len = numbers.length;
+		memo = new int[len][len];
+		System.out.println(LISMemo(numbers, 0, len, -1));
+	}
+
+}
